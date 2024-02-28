f174e62bfad8bd284cf30193d65ff37577765f77
Paul Giovanni <68184684+PaulGi48@users.noreply.github.com>
2024-02-28 14:27:14 +0100

    pandq

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>GitStats Explorer</h1>
+      {/* Additional header content */}
+    </header>
+  );
+}
+
+export default Header;
